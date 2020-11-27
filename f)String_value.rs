9362f62f3115ef303085919ec3502093f6a02c73
joy dhangra <joydhangra@gmail.com>
2020-11-28 01:31:36 +0600

    Create f)String_value.rs

---
+++ f)String_value.rs
@@ -0,0 +1,12 @@
+fn main() {
+    hello_world("nusrat");
+    add(4, 5);
+}
+
+fn hello_world(name: &str){
+    println!("I wanna kiss you {}", name)
+}
+
+fn add(x: 18, y:18) {
+    println!("{}", x + y);
+}
